bba3416a066739f1c3fa0cb1225e6f59bedca6fe
bibashmanjusubedi <acharabibash@outlook.com>
2026-01-01 14:55:09 +0545

    feat: FAQ Section

--- script.js
+++ script.js
@@ -22,3 +22,20 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 });
+
+
+const faqItems = document.querySelectorAll('.faq-item');
+
+faqItems.forEach(item => {
+    const btn = item.querySelector('.faq-question');
+
+    btn.addEventListener('click',() => {
+        //close others (Netflix style - one open at a time)
+        faqItems.forEach(i => {
+            if(i != item) i.classList.remove('active');
+        });
+
+        // toggle current
+        item.classList.toggle('active');
+    });
+});
